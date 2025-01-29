8c3f005513873d1c056b16723e866543090a0409
Jeff Yeazell <jyeazell@mac.com>
2025-01-29 12:19:14 -0800

    Get flows function

--- get-flows-sandbox.R
+++ get-flows-sandbox.R
@@ -8,27 +8,33 @@ cdec_stations <- c("SRM", # Shasta R. near Montague
                    "SJF" # Scott R. near Fort Jones"
 )
 
-cdec_sensor <- 20 # Sensor ID
+sensor <- 20 # Sensor ID
+
 
 
 # Define the function to get the latest row of data
-getCdecFlows <- function(station = "SJF", sensor = 20) {
+getCdecFlow <- function(station = station, sensor = sensor) {
+
   # Retrieve the data from the CDER API
-  data <- cder::cdec_query(station = station, sensor = sensor)
+  x <- cder::cdec_query(station = station, sensor = sensor)
 
   # Check if data is retrieved
-  if (nrow(data) == 0) {
+  if (nrow(x) == 0) {
     stop("No data available for the specified station and sensor.")
   }
 
   # Filter for the latest row
-  latest_data <- data %>%
-    arrange(desc(datetime)) %>%
+  x <- x %>%
+    filter(!is.na(Value)) %>%
+    arrange(desc(DateTime)) %>%
     slice(1)
 
   return(latest_data)
 }
 
+
+
+
 # Call the function
-latest_flow <- getCdecFlows()
+latest_flow <- getCdecFlow()
 print(latest_flow)

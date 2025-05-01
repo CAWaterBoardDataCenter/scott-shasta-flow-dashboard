726bcd029eeebb6efc526fbce99f3d24c26fb59c
Jeff Yeazell <jyeazell@mac.com>
2025-05-01 14:17:12 -0700

    Got it. I need to observe input$map_groups, not input$base_layer

--- app-border-straightR-updated.R
+++ app-border-straightR-updated.R
@@ -1,53 +1,32 @@
+
 library(shiny)
 library(leaflet)
 library(sf)
 
 # Load geojson file from data folder.
-watershedBoundaries <- st_read("./data/scott-shasta-huc8s/scott-shasta-huc8s.geojson")# %>%
-#  st_transform('+proj=longlat +datum=WGS84')
-
+st_read("./data/scott-shasta-huc8s/scott-shasta-huc8s.geojson") %>%
+  st_transform('+proj=longlat +datum=WGS84')
 
 ui <- fluidPage(
-  # Display the Leaflet map.
   leafletOutput("map", height = 500)
 )
 
 server <- function(input, output, session) {
-  # Define the coordinates for the square polygon.
-
-
-  # Render the initial map.
   output$map <- renderLeaflet({
     leaflet() %>%
-
-      # Add provider tiles for different basemaps.
       addProviderTiles(providers$Esri.WorldStreetMap, group = "Street") %>%
       addProviderTiles(providers$Esri.WorldTopoMap, group = "Topographic") %>%
       addProviderTiles(providers$Esri.WorldImagery, group = "Aerial") %>%
-
-      # Enable layer control to allow switching between basemaps.
       addLayersControl(
         baseGroups = c("Street", "Topographic", "Aerial"),
         options = layersControlOptions(collapsed = FALSE)
-      ) %>%
-
-      # Add watershed boundary polygons.
-      addPolygons(
-        data = watershedBoundaries,
-        color = NA,  # Black outline for the topographic view.
-        weight = 2,
-        fill = FALSE,
-        group = "watershedGroup"
-       )
-
+      )
   })
 
-  # Observe basemap changes. The leaflet package creates a reactive input named
-  # input$map_baselayer that reflects the name of the current base layer.
   observe({
     req(input$map_groups)
 
-    color <- if (input$map_groups == "Aerial") "white" else "black"
+    color <- if ("Aerial" %in% input$map_groups) "white" else "black"
 
     leafletProxy("map") %>%
       clearGroup("watershedGroup") %>%
@@ -59,8 +38,6 @@ server <- function(input, output, session) {
         group = "watershedGroup"
       )
   })
+}
 
-  }
-
-# Run the Shiny app.
 shinyApp(ui, server)
